715f9720211beb7fb94e45b7495b4c46def925eb
AliomarADI <aliomaramerica@gmail.com>
2022-06-14 11:02:51 +0600

    Merged

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:vapeteka/presentation/pages/application.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
