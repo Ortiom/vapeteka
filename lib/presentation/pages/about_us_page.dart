edfd62c6c56c62512eb04293cbe4c25d73280d35
aram-nyan <aram.maluha@gmail.com>
2022-06-14 11:58:41 +0600

    hotfix

--- lib/presentation/pages/about_us_page.dart
+++ lib/presentation/pages/about_us_page.dart
@@ -1,4 +1,3 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:url_launcher/url_launcher.dart';
